2036b2c5cf94cb79efc51b0676b44d19eb49aa70
Cihan B <cihangirb@hotmail.com>
2016-08-08 16:43:39 -0700

    lowered the initial sample load size.

--- load_sample_data.sh
+++ load_sample_data.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # load 1M items with 100K value size with a1 having 100 distinct values, using 10 threads in parallel 
-/usr/local/bin/python3.5 LoadGenCouchbase.py -hn=couchbase://localhost/default -op=load -kp=A -ks=0 -ke=2000000 -vs=100 -sl=100 -tc=10
+/usr/local/bin/python3.5 LoadGenCouchbase.py -hn=couchbase://localhost/default -op=load -kp=A -ks=0 -ke=100 -vs=100 -sl=100 -tc=10
